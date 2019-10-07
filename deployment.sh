e8d899b0ba79ae49e6a383d1504e5859f72f2197
Ubuntu <mustafa@newNode.ympvdzqrkkverh0cmq00jyfp1g.dxbx.internal.cloudapp.net>
2019-10-07 15:57:05 +0000

    update scripts

--- deployment.sh
+++ deployment.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-#1st check if a new .jar befoe create new container
-#Assuming that the script argument is put in a form "dir/jar_file"
-
-Work_dir=$(echo $1 | cut -d/ -f1 ) 
-echo $Work_dir
-Current_jar=$(grep "ENV" $Work_dir/Dockerfile | awk '{print $3}')
-echo $Current_jar
-new_jar=$(echo $1 | cut -d/ -f2 )
-echo $new_jar
-if [ "$Current_jar" == "$new_jar" ]; 
-then
-	echo "u r in"
-else
-  sed -i "s/$Current_jar/$new_jar/g" ./$Work_dir/Dockerfile
-  echo "jar file has been updated"
-  echo "App container will be updated"
-
-## Get Container Name & Version
-  DesiredApp=$(echo $Work_dir|cut -c1-7)
-  cont=$(docker ps -f name=$DesiredApp -q) 
-  cont_f_name=$(docker inspect "$cont" -f "{{ .Name }}"|cut -d/ -f2)
-  cont_name=$(docker inspect "$cont" -f "{{ .Name }}"|cut -d/ -f2|cut -d "." -f1)
-  cont_version=$(docker inspect "$cont" -f "{{ .Name }}"|cut -d/ -f2|cut -d "." -f2)
-  cont_ver_num=$(echo $cont_version|cut -c2)
-  echo "Current runnign cont is: $cont_f_name"
-
-## Get Image Name & Version
-  Cur_img=$(docker inspect $cont_f_name -f "{{.Config.Image}}")
-  image_name=$(docker inspect $cont_f_name -f "{{.Config.Image}}"|cut -d: -f1)
-  image_ver=$(docker inspect $cont_f_name -f "{{.Config.Image}}"|cut -d: -f2)  
-  img_ver_num=$(echo $image_ver|cut -c2)
-  echo $img_ver_num
-
-  #cp docker-compose_v1.yml docker-compose_v$(($version_num+1)).yml
-   #sed -i "s/$image_name:$image_ver/$image_name:"
-
 ## Get container network
   cont_net=$(docker inspect $cont_f_name -f "{{json .NetworkSettings.Networks }}"|cut -d '"' -f2)
 
@@ -66,15 +30,16 @@ EOF
           docker exec -i nginxproxy sed -i "s/$cont_f_name:8080/$cont_name.v$(($cont_ver_num+1)):8080/g" /etc/nginx/nginx.conf
 
 ## Restart the nginx service to redirect the request to the new container.
-          docker restart nginxproxy 
+          docker exec nginxproxy nginx -s reload
  
 ## Remove old container
           docker stop $cont_f_name && docker rm $cont_f_name
 
 ## Update the current compose file to update all the stack
-          sed -i "s/$cont_f_name/$cont_name.v$(($cont_ver_num+1))/g" docker-compose.yml
-	  sed -i "s/$Cur_img/$image_name:v$(($img_ver_num+1))/g" docker-compose.yml
+          echo $Cur_img
+          sed -i "
+	  s/$cont_f_name/$cont_name.v$(($cont_ver_num+1))/g 
+	  s/$Cur_img/$image_name:v$(($img_ver_num+1))/g" docker-compose.yml
 	  rm -f $cont_name.v$(($cont_ver_num+1))-compose.yml
   fi
 
-fi
--- testNewBuild.sh
+++ testNewBuild.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-#1st check if a new .jar befoe create new container
-#Assuming that the script argument is put in a form "dir/jar_file"
-
-if [ -n $1 ];then
-
-  export Work_dir=$(echo $1 | cut -d/ -f1 )
+## Check .jar file befoe create new container
+### Assuming that the script argument is put in a form "./dir/jar_file"
+IFS=$'\n'
+if [ -n $1 -a -e $1 ];then
+### Check if the argument make will make update to Dockerfile or not!
+  export Work_dir=$(echo $1 | cut -d/ -f2 )
    echo $Work_dir
   export Current_jar=$(grep "ENV" $Work_dir/Dockerfile | awk '{print $3}')
    echo $Current_jar
-  export new_jar=$(echo $1 | cut -d/ -f2 )
+  export new_jar=$(echo $1 | cut -d/ -f3 )
    echo $new_jar
   if [ "$Current_jar" == "$new_jar" ];
   then
         echo "Dockerfile already updated"
   else
-   # sed -i "s/$Current_jar/$new_jar/g" ./$Work_dir/Dockerfile
-   # echo "jar file has been updated"
-## Get Container Name & Version
+## Get Container Name & Version or runnig Images
+
   DesiredApp=$(echo $Work_dir|cut -c1-7)
   cont=$(docker ps -f name=$DesiredApp -q|sed -n '1p' )
   export cont_f_name=$(docker inspect "$cont" -f "{{ .Name }}"|cut -d/ -f2)
@@ -25,29 +24,32 @@ if [ -n $1 ];then
   export cont_version=$(docker inspect "$cont" -f "{{ .Name }}"|cut -d/ -f2|cut -d "." -f2)
   export cont_ver_num=$(echo $cont_version|cut -c2)
   echo "Current runnign cont is: $cont_f_name"
-  #Running_Jfile=$(docker exec -i $cont_f_name bash -c env|grep APP|cut -d '=' -f2)
   echo $Running_Jfile
+
 ## Get Image Name & Version
   Cur_img=$(docker inspect $cont_f_name -f "{{.Config.Image}}")
   export image_name=$(docker inspect $cont_f_name -f "{{.Config.Image}}"|cut -d: -f1)
   export image_ver=$(docker inspect $cont_f_name -f "{{.Config.Image}}"|cut -d: -f2)
   export img_ver_num=$(echo $image_ver|cut -c2)
   echo $img_ver_num
-## Check if .jar file has been included in any previous images
+
+## Check if the new jar file has been included in any previous images
+  
+  arr=()
   for i in $(seq 1 $img_ver_num); do
       echo $i
       echo $image_name
       echo $image_name:v$i
-      if ! [[ `docker inspect "$image_name:v$i" -f "{{.Config.Env}}" | grep $new_jar` ]]; then
-	     # echo "$new_jar has been included in image before"
-	     sed -i "s/$Current_jar/$new_jar/g" ./$Work_dir/Dockerfile
-	      echo "jar file has been updated"
-	      bash -x ./deployment.sh2
-      else
-	    # bash -x ./deployment.sh2
-	    echo "$new_jar has been included in image before" 
-      fi
+      arr+=(`docker inspect "$image_name:v$i" -f "{{.Config.Env}}"`)
   done
+  echo ${arr[@]} | tr ' ' '\n' | grep  "$new_jar"
+    if [ $? -eq 0 ]; then 
+  	   echo "already Configred"
+    else  
+       	  sed -i "s/$Current_jar/$new_jar/g" ./$Work_dir/Dockerfile
+           echo "jar file has been updated"
+           bash -x ./deployment.sh2
 
+    fi
   fi
 fi
